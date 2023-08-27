cab10a4b33f1d66bceb593ea548d8229eaf5bb43
Kira Pitman <kirapitman@gmail.com>
2023-08-27 19:02:54 +1200

    start project

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react'
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <h1>React and Knex Boilerplate</h1>
-    </div>
+    <>
+      <div>
+        <h1>Trail Running 2: Electric Boogaloo</h1>
+      </div>
+      <div className="count">{count}</div>
+      <div className="button">
+        <button onClick={() => setCount(count + 1)}>Click meeeeeee</button>
+      </div>
+    </>
   )
 }
 
